a87e0779a356a847aabfae4db53cb0c01f77b3da
Sura <sura@MacBook-Pro-de-Stagiaire.local>
2019-10-31 18:08:45 +0100

    add remove and validation interaction

--- assets/components/organisms/add-commentary/add-commentary.js
+++ assets/components/organisms/add-commentary/add-commentary.js
@@ -1,5 +1,13 @@
 export default () => {
   $('#button-justify').on('click', function() {
-    $('#add-time').addClass('open');
+    $('#add-commentary').addClass('open');
+  });
+
+  $('#close-commendary').on('click', function() {
+    $('#add-commentary').removeClass('open');
+  });
+
+  $('#validation').on('click', function() {
+    $('#add-commentary').removeClass('open');
   });
 };
