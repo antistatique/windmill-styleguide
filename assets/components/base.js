8081466124f56ac15715ced7fd3c8aaf3794086e
Sura <sura@MacBook-Pro-de-Stagiaire.local>
2019-10-31 18:10:02 +0100

    fix somes details

--- assets/components/base.js
+++ assets/components/base.js
@@ -2,12 +2,14 @@
 import svgIcons from '../icons/svg-icons';
 
 import addCommentary from './organisms/add-commentary/add-commentary';
+import addTime from './organisms/add-time/add-time';
 
 svgIcons(); // Must run as soon as possible
 
 const init = () => {
   // Run your imported scripts
   addCommentary();
+  addTime();
 };
 
 (function($) {
