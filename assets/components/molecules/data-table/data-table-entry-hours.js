5c4497f5da75cb4864fc078ee3b9572bd8528ea5
Sura <sura@MacBook-Pro-de-Stagiaire.local>
2019-11-04 13:59:18 +0100

    fix table

--- assets/components/molecules/data-table/data-table-entry-hours.js
+++ assets/components/molecules/data-table/data-table-entry-hours.js
@@ -1,5 +1,13 @@
 export default () => {
   $('#reset').on('click', function() {
-    $('#zero').replace('0');
+    $('.zero').each(function() {
+      $(this).text('00:00');
+    });
+  });
+
+  $('#usual-schedule').on('click', function() {
+    $('.number').each(function() {
+      $(this).text('09:00');
+    });
   });
 };
